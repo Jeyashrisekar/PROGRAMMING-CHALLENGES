b1e926aaf463157984e5f1989e766c33590467a0
Jeyashrisekar <168284800+Jeyashrisekar@users.noreply.github.com>
2026-02-16 17:28:46 +0530

    Time: 1 ms (99.65%), Space: 43.7 MB (54.32%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public static String addBinary(String a, String b) {
+        StringBuilder res = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, c = 0;
+
+        while (i >= 0 || j >= 0 || c != 0) {
+            int sum = c;
+
+            if (i >= 0){
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            res.append(sum % 2);      
+            c = sum / 2;            
+        }
+
+        return res.reverse().toString();
+    
+    }
+}
