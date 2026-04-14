6e6c7d9e60bd7e42581529b468f88c3a68707a85
Jeyashri S <168284800+Jeyashrisekar@users.noreply.github.com>
2026-04-14 22:17:29 +0530

    Time: 0 ms (100%), Space: 42.4 MB (77.44%) - LeetHub

--- 1185-day-of-the-week/1185-day-of-the-week.java
+++ 1185-day-of-the-week/1185-day-of-the-week.java
@@ -8,12 +8,16 @@ class Solution {
         int d = day;
         for (int i = 1; i < month; i++)  d += m[i];
         
-        if ((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0)) && month > 2)  d++;
+        if (isLeapYear(year) && month > 2)  d++;
 
         year--;
         d += (year + (year / 4) - (year / 100) + (year / 400)) % 7;
 
         return s[(d) % 7];
+    }
+
+    private boolean isLeapYear(int year) {
+        return (year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0));
     }
 
 }
