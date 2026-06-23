6b67501fc9167681efcd8022bcaf03930879611b
Jeyashri S <168284800+Jeyashrisekar@users.noreply.github.com>
2026-06-24 00:38:30 +0530

    Time: 37 ms (49.57%), Space: 73.3 MB (36.71%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,38 +1,46 @@
-
 class Solution {
-
+    static int[][]arr;
+    static int []dp;
+    static int n ;
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+         n=profit.length;
+           arr=new int[n][3];
+           for(int i=0;i<n;i++){
+            arr[i][0]=startTime[i];
+            arr[i][1]=endTime[i];
+            arr[i][2]=profit[i];
+           }
+           dp=new int[n+1];
+           Arrays.fill(dp,-1);
+           Arrays.sort(arr,(a,b)->a[0]-b[0]);
+           return solve(0);
 
-        int n = startTime.length;
-
-        int[][] jobs = new int[n][3];
-
-        for(int i = 0; i < n; i++) {
-            jobs[i][0] = startTime[i];
-            jobs[i][1] = endTime[i];
-            jobs[i][2] = profit[i];
+    }
+    public int solve(int i){
+        if(i>=n){
+            return 0;
         }
-
-        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
-
-        TreeMap<Integer, Integer> dp = new TreeMap<>();
-
-        dp.put(0, 0);
-
-        for(int[] job : jobs) {
-
-            int start = job[0];
-            int end = job[1];
-            int p = job[2];
-
-            int cur =
-                dp.floorEntry(start).getValue() + p;
-
-            if(cur > dp.lastEntry().getValue()) {
-                dp.put(end, cur);
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int next=getNextIndex(i+1,arr[i][1]);
+            int take=arr[i][2]+solve(next);
+            int nottake=solve(i+1);
+        return dp[i]=Math.max(take,nottake);
+    }
+    public int getNextIndex(int lo,int currentjobend){
+        int high=n;
+       
+        while(lo<high){
+             int mid=lo+(high-lo)/2;
+            if(arr[mid][0]>=currentjobend){
+                high=mid;
             }
+            else{
+                lo=mid+1;
+            }
+            
         }
-
-        return dp.lastEntry().getValue();
+        return lo;
     }
 }
