fc61e354ced9a0966d697fbcc78fd84405bcca22
Jeyashri S <168284800+Jeyashrisekar@users.noreply.github.com>
2026-06-24 13:28:06 +0530

    Time: 11 ms (93.75%), Space: 99 MB (83.39%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -1,21 +1,25 @@
 class Solution {
-   public int longestCycle(int[] edges) {
-    int res = -1;
-    boolean[] vis = new boolean[edges.length]; // global visisted
+  public int longestCycle(int[] edges) {
+    int ans = -1; // Initialize the answer to -1
+    int time = 1; // Initialize the current time step to 1
+    int[] timeVisited = new int[edges.length]; // Initialize an array to store the time at which each node was first visited
 
-    for(int i=0; i<edges.length; i++){
-        if(vis[i]) continue;
-        HashMap<Integer, Integer> x = new HashMap<>();  // local visited
-        for (int idx=i, dist=0; idx!=-1; idx=edges[idx]){
-            if(x.containsKey(idx)){
-                res = Math.max(res, dist-x.get(idx));
-                break;
-            }
-            if(vis[idx]) break;
-            vis[idx] = true;
-            x.put(idx, dist++);
-        } 
+    // Iterate through each node in the graph
+    for (int i = 0; i < edges.length; ++i) {
+      if (timeVisited[i] > 0) // If the node has already been visited, skip it
+        continue;
+      final int startTime = time; // Record the start time of the current traversal
+      int u = i; // Initialize the current node to the ith node
+      // Traverse the graph until the end of the path is reached or a visited node is encountered
+      while (u != -1 && timeVisited[u] == 0) {
+        timeVisited[u] = time++; // Record the current time step and increment time
+        u = edges[u]; // Move to the next node in the path
+      }
+      // If a cycle is found that includes the current node, update the answer
+      if (u != -1 && timeVisited[u] >= startTime)
+        ans = Math.max(ans, time - timeVisited[u]);
     }
-    return res;
-}
+
+    return ans; // Return the length of the longest cycle found
+  }
 }
