d10cf6e0c0ccebda8a78a5656da6afef58fc1eaa
Jeyashri S <168284800+Jeyashrisekar@users.noreply.github.com>
2026-07-23 10:10:53 +0530

    Time: 806 ms (74.4%), Space: 0B (100%) - LeetHub

---
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -0,0 +1,4 @@
+SELECT name, SUM(Transactions.amount) AS balance FROM Users 
+JOIN Transactions ON Users.account = Transactions.account 
+GROUP BY Users.account
+HAVING SUM(Transactions.amount) > 10000;
