a37a0d5b1f0b0679834b04c308bc5bee70109126
Jeyashri S <168284800+Jeyashrisekar@users.noreply.github.com>
2026-07-29 15:28:55 +0530

    Time: 382 ms (10.06%), Space: 42.7 MB (32.3%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -0,0 +1,25 @@
+class Solution {
+    int min = Integer.MAX_VALUE;
+    public int distributeCookies(int[] cookies, int k) {
+        int[] c = new int[k];
+        distribute(0, c, cookies, k);
+        return min;
+    }
+    public void distribute(int i , int[] c, int[] cookies, int k){
+        if(i == cookies.length)
+        {
+            int max = Integer.MIN_VALUE;
+            for(int x : c){
+                max = Math.max(max, x);
+            }
+            min = Math.min(min, max);
+            return;
+        }
+        for(int j = 0; j < k; j++){
+            c[j] += cookies[i];
+            distribute(i+1, c, cookies, k);
+            c[j]-=cookies[i];
+        }
+
+    }
+}
