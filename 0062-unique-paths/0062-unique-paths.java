9dbaede036f52ac53c1f7bb04e7de7ddfcdee825
Jeyashri S <168284800+Jeyashrisekar@users.noreply.github.com>
2026-08-06 15:29:58 +0530

    Time: 1 ms (14.45%), Space: 42.1 MB (66.78%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,19 +1,29 @@
 class Solution {
-    static int[][] dp;
     public int uniquePaths(int m, int n) {
-        dp = new int[m][n];
-        for(int[] i : dp){
-            Arrays.fill(i, -1);
+        //memoization
+        int[][] dp = new int[m][n];
+        for(int[] r : dp){
+            Arrays.fill(r, -1);
+           
         }
-        int res = count(0, 0, m, n);
-        return res;
-
+         return paths(m-1, n-1, dp);
+        
+    
     }
-    static int count(int i , int j, int m, int n){
-        if(i >= m || j >= n) return 0;
-        if(i == m -1 && j == n-1) return 1;
+    public int paths(int i, int j, int[][] dp){
+        if(i < 0 || j < 0) return 0;
+        if(i == 0 && j == 0) return 1;
         if(dp[i][j] != -1) return dp[i][j];
-        dp[i][j] = count(i+1, j, m, n)+count(i, j+1, m, n);
+        int l = paths(i, j -1, dp);
+        int u = paths(i-1, j, dp);
+        dp[i][j] = l + u;
         return dp[i][j];
     }
 }
+// public int paths(int i, int j){  ----> i= m-1, j = n-1
+//         if(i < 0 || j < 0) return 0;
+//         if(i == 0  && j == 0) return 1;
+//         int l = paths(i, j -1);
+//         int u = paths(i-1, j);
+//         return l+u;
+//}
