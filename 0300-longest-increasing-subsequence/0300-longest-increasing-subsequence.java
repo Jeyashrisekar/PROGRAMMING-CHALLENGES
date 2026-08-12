96f667ff8fc688ff435d93ed7acb601a8a96cff4
Jeyashri S <168284800+Jeyashrisekar@users.noreply.github.com>
2026-08-12 12:39:44 +0530

    Time: 352 ms (16.14%), Space: 237.5 MB (5.27%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n-1];
+        for(int[] r : dp) Arrays.fill(r, -1);
+        return lis(0, -1, nums, dp);
+
+    }
+    public int lis(int i, int prev, int[] nums, int[][] dp){
+        if(i == nums.length) return 0;
+        if(prev != -1 && dp[i][prev] != -1) return dp[i][prev];
+        int pick = 0;
+        if(prev == -1 || nums[i] > nums[prev]){ //prev -1 means lis is empty
+            pick = 1 + lis(i+1, i, nums, dp);
+        }
+        int np = lis(i+1, prev, nums, dp);
+        if(prev!= -1) return dp[i][prev] = Math.max(pick, np);
+
+        else return Math.max(pick, np);
+    }
+}
