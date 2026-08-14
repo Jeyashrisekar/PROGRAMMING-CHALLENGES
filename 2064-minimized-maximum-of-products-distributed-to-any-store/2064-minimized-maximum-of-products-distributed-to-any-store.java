6296ef89923be147a09ea32c8074be52497da22e
Jeyashri S <168284800+Jeyashrisekar@users.noreply.github.com>
2026-08-14 11:57:44 +0530

    Time: 89 ms (16.86%), Space: 81.1 MB (75.2%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        int t = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i : quantities){
+           if(max < i) max = i;
+        }
+        int low = 1;
+        int high = max;
+        int ans = -1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(isItpossible(mid, quantities, n)){
+                ans = mid;
+                high = mid -1;
+            }
+            else low = mid +1;
+
+        }
+        return ans;
+
+    }
+    public boolean isItpossible(int mid, int[] quantities, int totalstores){
+        int storesneeded = 0;
+        for(int i = 0; i < quantities.length; i++){
+            storesneeded += (int) Math.ceil((double) quantities[i]/mid);
+        }
+        return (storesneeded <= totalstores);
+
+    }
+}
